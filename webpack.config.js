724b120d0a8063576b91339686541ba63fddc42a
Andre <andre.lls@hotmail.com>
2020-09-11 18:39:29 -0400

    Finish setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+// copy and paste!
+module.exports = {
+  context: __dirname,
+  entry: './frontend/whatever_note.jsx',
+  output: {
+    path: path.resolve(__dirname, ''),
+    filename: './app/assets/javascripts/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
