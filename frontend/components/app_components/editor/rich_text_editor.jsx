8969475749631f32f2027799a4adfae111ba7d02
Andre <andre.lls@hotmail.com>
2020-09-29 23:57:12 -0400

    Finish initial setup of editor toolbar

--- frontend/components/app_components/editor/rich_text_editor.jsx
+++ frontend/components/app_components/editor/rich_text_editor.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import ReactQuill from "react-quill";
-import EditorToolbar, { modules, formats } from "./editor_toolbar";
-// import {MyToolbar, formats, modules} from './my_toolbar';
+import {MyToolbar, formats, modules} from './editor_toolbar';
 
 
 export default class RichTextEditor extends React.Component{
     constructor(props){
         super(props);
         this.state = {body: "", title: ""};
-        this.handleChange = this.handleChange.bind(this);
+        this.handleBodyChange = this.handleBodyChange.bind(this);
     }
 
-    handleChange(field){
-        return e => this.setState({[field]: e.target.value})
+    handleBodyChange(body){
+        console.log(this.state.body);
+        this.setState({body})
     }
 
     render(){
         return(
             <div className="editor">
-                <EditorToolbar />
-                {/* <MyToolbar/> */}
+                {/* <EditorToolbar /> */}
+                <MyToolbar/>
                 <h1>Hellor there</h1>
                 <ReactQuill value={this.state.body}
-                            onChange={this.handleChange('body')}
-                            // theme="snow"
+                            onChange={this.handleBodyChange}
+                            theme="snow"
                             modules={modules}
                             formats={formats}
-                            />
+                />
                 
             </div>
         )
