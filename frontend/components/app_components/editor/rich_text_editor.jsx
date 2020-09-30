7555bf4ca4a544e94719b675ab58f25d7cef46f0
Andre <andre.lls@hotmail.com>
2020-09-30 12:21:35 -0400

    Lunch break

--- frontend/components/app_components/editor/rich_text_editor.jsx
+++ frontend/components/app_components/editor/rich_text_editor.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactQuill from "react-quill";
 import {MyToolbar, formats, modules} from './editor_toolbar';
 import HeaderContainer from "./header_container";
+import Footer from './footer'
 
 
 export default class RichTextEditor extends React.Component{
@@ -13,6 +14,7 @@ export default class RichTextEditor extends React.Component{
         })
         this.handleBodyChange = this.handleBodyChange.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.toolbarToggle = this.toolbarToggle.bind(this);
     }
 
     handleBodyChange(body){
@@ -23,10 +25,17 @@ export default class RichTextEditor extends React.Component{
         this.setState({title: e.target.value})
     }
 
+    toolbarToggle(){
+        // debugger
+        const eles = document.getElementsByClassName('toolbar-field')[0].childNodes;
+        eles.forEach(ele=>{
+            ele.classList.toggle('active');
+        })
+    }
+
     render(){
         return(
             <div className="rich-text-editor">
-                <div className='header'>
                     <HeaderContainer note={{author_id: 5,
                                             body: "Alright this looks pretty good to me",
                                             created_at: "2020-09-29T17:11:54.300Z",
@@ -34,17 +43,22 @@ export default class RichTextEditor extends React.Component{
                                             notebook_id: 13,
                                             title: "Cool cool cool",
                                             updated_at: "2020-09-29T17:14:43.923Z"}}/>
+                <div className='toolbar-field'>
                     <p className="display active">Last edited on somedate</p>
-                    <MyToolbar/>
+                    <MyToolbar />
                 </div>
-                <input type="text" className="title" value={this.state.title} onChange={this.handleChange}/>
-                <ReactQuill value={this.state.body}
-                            onChange={this.handleBodyChange}
-                            theme="snow"
-                            modules={modules}
-                            formats={formats}
-                />
-                
+                <form>
+                    <input type="text" className="title" value={this.state.title} onChange={this.handleChange}/>
+                    <ReactQuill value={this.state.body}
+                                onChange={this.handleBodyChange}
+                                onFocus={this.toolbarToggle}
+                                onBlur={this.toolbarToggle}
+                                theme="snow"
+                                modules={modules}
+                                formats={formats}
+                    />
+                </form>
+                <Footer status={this.state.status} />
             </div>
         )
     }
